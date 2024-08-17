cca02d3d13acc8eb465cb6752fbbcbe37df156d6
b0c-beep <bocmarioadrian98@gmail.com>
2024-08-17 17:06:17 +0300

    idk what im doing

--- fragrance-watcher/src/App.jsx
+++ fragrance-watcher/src/App.jsx
@@ -1,12 +1,33 @@
 import './App.css'
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [prices, setPrices] = useState({});
+
+  useEffect(() => {
+      fetch('http://127.0.0.1:5000/api/prices')
+          .then(response => response.json())
+          .then(data => setPrices(data))
+          .catch(error => console.error('Error fetching prices:', error));
+  }, []);
 
   return (
-    <>
-      <h1>Fragrance Watcher</h1>
-    </>
-  )
+      <div>
+          <h1>Fragrance Prices</h1>
+          {Object.keys(prices).map(fragrance => (
+              <div key={fragrance}>
+                  <h2>{fragrance}</h2>
+                  <ul>
+                      {Object.keys(prices[fragrance]).map(site => (
+                          <li key={site}>
+                              {site}: {prices[fragrance][site] || 'Price not found'}
+                          </li>
+                      ))}
+                  </ul>
+              </div>
+          ))}
+      </div>
+  );
 }
 
 export default App
