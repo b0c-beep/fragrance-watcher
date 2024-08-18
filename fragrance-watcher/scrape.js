92a0513e1fbb78b7a085727ac5475e5982762c77
b0c-beep <bocmarioadrian98@gmail.com>
2024-08-18 21:28:49 +0300

    making progress on sephorafetch

--- fragrance-watcher/scrape.js
+++ fragrance-watcher/scrape.js
@@ -22,46 +22,30 @@ const sephoraFetch = async (page, store, details, fragrance, quantity) => {
     try {
         await page.goto(details.url, { waitUntil: 'domcontentloaded', timeout: 60000 });
 
+        const cookieAccept = await page.$('#footer_tc_privacy_button_3');
+        if (cookieAccept) {
+            await cookieAccept.click();
+        }
+
         // Check if there is a dropdown element
         const dropdownElement = await page.$('.variations-size-selected');
         if (dropdownElement) {
             // Toggle the dropdown to reveal all options
             await dropdownElement.click();
-            console.log('Dropdown clicked');
         }
 
+        
         // Find the correct variation based on quantity
-        const selectedPrice = await page.evaluate((quantity, priceSelector) => {
-            const variationElements = document.querySelectorAll('.variation-button-line');
-            let selectedPrice = 'Price not found';
+        const selectedPrice = await page.evaluate(async (quantity, priceSelector) => {
+            const variationElement = document.querySelector('.variation-button-line:nth-of-type(4)');
 
-            let specificSpan = "";
-
-            variationElements.forEach(variation => {
-
-                const spans = variation.querySelectorAll('.variation-title span');
-
-                
-
-                spans.forEach(span => {
-                    if (!span.id && !span.className) {
-                        specificSpan = span;
-                    }
-                });
-
-                /*
-                if (specificSpan.includes(parseInt(quantity))) {
-                    selectedPrice = variation.querySelector(priceSelector).innerText.trim();
-                }
-                */
-            });
+            await variationElement.click();   
             
-            return specificSpan;
-            //return selectedPrice;
+        
+            return selectedPrice;
         }, quantity, details.price_selector);
-
-        console.log("Selected price: ", selectedPrice);
-        //console.log(`Price at ${store} for ${fragrance}: ${selectedPrice}`);
+        
+        console.log(`Price at ${store} for ${fragrance}: ${selectedPrice}`);
     } catch (error) {
         console.error(`Error fetching price from ${store} for ${fragrance}:`, error);
     }
@@ -74,7 +58,7 @@ const fetchPrices = async () => {
         const fragrances = JSON.parse(data);
         
         // Launch the browser
-        const browser = await puppeteer.launch({ headless: true, defaultViewport: null });
+        const browser = await puppeteer.launch({ headless: false, defaultViewport: null, slowMo: 300 });
         const page = await browser.newPage();
 
         // Iterate through each fragrance
@@ -89,7 +73,8 @@ const fetchPrices = async () => {
                     if (store !== 'sephora.ro' && store !== 'douglas.ro' 
                         && store !== 'notino.ro' && store !== 'marionnaud.ro'
                         && store !== 'parfumuri-timisoara.ro') {
-                        await basicFetch(page, store, details, fragrance, quantity);
+
+                        //await basicFetch(page, store, details, fragrance, quantity);
                     } else if (store === 'sephora.ro') {
                         await sephoraFetch(page, store, details, fragrance, quantity);
                     }
